238b40cb19eaedde959dd4e50d401dfba81f5295
bsui <bsui@g-wearables.com>
2015-09-16 17:12:42 +0800

    修正

--- www/js/app.js
+++ www/js/app.js
@@ -5,13 +5,30 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'pascalprecht.translate','starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ngCordova.plugins.toast','oc.lazyLoad', 'pascalprecht.translate','starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$ionicPopup,$rootScope, $location,$timeout, $ionicHistory,$cordovaToast) {
+	 function showConfirm() {
+    var confirmPopup = $ionicPopup.confirm({
+        title: '<strong>退出应用?</strong>',
+        template: '你确定要退出应用吗?',
+        okText: '退出',
+        cancelText: '取消'
+    });
+    confirmPopup.then(function (res) {
+        if (res) {
+            ionic.Platform.exitApp();
+        } else {
+            // Don't close
+        }
+    });
+  }
+	//注意：$cordovaToast是消息提示框，若要使用需要安装才支持。命令cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
@@ -21,6 +38,20 @@ angular.module('starter', ['ionic', 'pascalprecht.translate','starter.controller
       StatusBar.styleLightContent();
     }
   });
+	//双击退出
+	$ionicPlatform.registerBackButtonAction(function (e) {
+			e.preventDefault();
+	    //判断处于哪个页面时退出
+	  	if($location.path()=='/app/index'){
+	  		showConfirm();
+	  	}else if($rootScope.$viewHistory.backView){
+				$rootScope.$viewHistory.backView.go();
+	  	}else{
+	  		showConfirm();
+	  	}
+
+	    return false;
+	}, 101);
 })
 
 .config(function($stateProvider, $urlRouterProvider,$translateProvider) {
@@ -94,7 +125,32 @@ angular.module('starter', ['ionic', 'pascalprecht.translate','starter.controller
 	    views: {
 	      'menuContent': {
 	        templateUrl: 'templates/charts/echarts.html',
-					//controller: 'FunctionlistCtrl'
+			controller: 'EChartCtrl',
+			/**
+			resolve: {
+			    data: function ($http) {
+			        return $http.get('ticket.action?method=projectTickets').then(function (data) {
+			            return data;
+			        }, function () {
+			            return {};
+			        });
+			    }
+			}
+			**/
+			//路由懒加载
+			/*
+    		resolve: {
+                EChartCtrl: function($ocLazyLoad){
+
+                    return $ocLazyLoad.load(
+                        {
+                            name: "starter.function.chartscontroller",  //controller所在的module name
+                            files: ["js/function/chartscontroller.js"]//文件位置
+                        }
+                    )
+                }
+        	}
+        	*/
 	      }
 	    }
 	  })
