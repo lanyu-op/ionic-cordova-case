3555fb17ebebbd6a7f110d3c1abaa48cbd5dd95b
bsui <bsui@g-wearables.com>
2015-09-22 10:21:25 +0800

    1

--- www111/app/controllers/Bracelet/SportSettingLangController.js
+++
@@ -1,8 +0,0 @@
-define(['app'], function (app) {
-app.controller('LanguageCtrl', function($scope,$translate) {
-	$scope.title="SelectLanguage_Lang";
-	$scope.changeLanguage = function (key) {
-	   $translate.use(key);
-	};
-});
-});
