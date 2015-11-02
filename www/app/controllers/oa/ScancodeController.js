489a0c661206675ae9a46f72195911454da5be10
bsui <bsui@g-wearables.com>
2015-11-02 21:57:32 +0800

    2

---
+++ www/app/controllers/oa/ScancodeController.js
@@ -0,0 +1,8 @@
+define(['app'], function (app) {
+	app.controller('ScancodeCtrl', function($ionicLoading,$scope,$ionicPopup,$http,$cordovaSQLite,$state,Upload,$rootScope,$stateParams,$timeout) {
+
+
+
+
+   });
+});
