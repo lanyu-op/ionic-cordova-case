c5813e9e89cad2b6b3bd3d108a0561b2f944e892
jetty <157595075@qq.com>
2015-11-10 14:28:15 +0800

    1
    2

--- www/app/controllers/oa/AmapController.js
+++ www/app/controllers/oa/AmapController.js
@@ -1,11 +1,14 @@
 define(['app'], function (app) {
-app.controller('amapCtrl', function(Amap,$ionicLoading,$scope,$rootScope,$ionicPopup,$http,$q) {
+app.controller('amapCtrl', function(Amap,$sce,$ionicLoading,$scope,$rootScope,$ionicPopup,$http,$q) {
   $scope.gogo=function(){
     alert(8);
+   // $sce.trustAsResourceUrl('http://www.baidu.com');
   }
+
+  $scope.url=$sce.trustAsResourceUrl('http://www.baidu.com');
   Amap.promise.then(function () {
     // do something after Amap API loaded
-
+//alert(6);
     var map = new AMap.Map('mapContainer');
     map.setZoom(12);
     map.setCenter([116.480983, 39.989628]);
@@ -18,13 +21,13 @@ app.controller('amapCtrl', function(Amap,$ionicLoading,$scope,$rootScope,$ionicP
       map.addControl(toolBar);
     });
     var infowindow = new AMap.InfoWindow({
-      content: '<h3>高德地图</h1><a href="http://www.baidu.com">高德是中国领先的数字地图内容、导航和位置服务解决方案提供商。</a>',
+      content: '<h3>高德地图</h1><a href="#" onclick="aaa()">高德是中国领先的数字地图内容、导航和位置服务解决方案提供商。</a>',
       offset: new AMap.Pixel(0, -30),
       size:new AMap.Size(230,0)
     });
     infowindow.open(map,new AMap.LngLat(116.480983, 39.989628));
 
-
+		//$scope.map=map;
 
   });
 
--- www/lib/angular-amap/angular-amap.js
+++ www/lib/angular-amap/angular-amap.js
@@ -16,17 +16,25 @@ angular.module('l42y.amap', [
       var deferred = $q.defer();
 
       function callback () {
+      	//console.log('加载成功！');
+      	//alert('加载成功！')
         deferred.resolve();
       }
 
       $window[reference] = callback;
       (function (document, script, scriptElement) {
         scriptElement = document.createElement(script);
-        scriptElement.src = $location.protocol() + '://' +
+        //$location.protocol()
+        scriptElement.src ='http://' +
           'webapi.amap.com/maps?v=' + provider.config.version +
           '&key=' + provider.config.key +
           '&callback=' + reference;
         document.body.appendChild(scriptElement);
+        //console.log('正在加载。。。。');
+       // alert('正在加载！'+$location.protocol() + '://' +
+       //   'webapi.amap.com/maps?v=' + provider.config.version +
+       //   '&key=' + provider.config.key +
+       //   '&callback=' + reference);
       }($window.document, 'script'));
 
       var service = {
