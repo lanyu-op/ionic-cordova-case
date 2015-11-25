d62a84e748c2ad90db5c5ad41b65111f286f773d
cn-suibin <157595075@qq.com>
2015-11-25 19:23:41 +0800

    jq dom使用

--- www/app/controllers/demo/DomcaseController.js
+++ www/app/controllers/demo/DomcaseController.js
@@ -3,11 +3,13 @@ define([
 
 ], function (app) {
   'use strict';
-//设备列表
+//DOM操作
 app.controller('DomcaseCtrl', function($scope,$compile) {
-     	
+     	$scope.test=function(){
+     		alert("这是填充dom的事件操作！");
+     	}
 //通过$compile动态编译html
-var html="<div ng-click='test()'><h1>222222222222222222</h1></div>";
+var html="<div ng-click='test()'><h1>这是填充进来的dom</h1></div>";
 var template = angular.element(html);
 var mobileDialogElement = $compile(template)($scope);
 angular.element(document).find($("ion-item")[0]).append(mobileDialogElement);
