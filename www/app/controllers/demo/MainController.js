a861afb582f660c0a8f97c9982c7f72ae03ad6be
cn-suibin <157595075@qq.com>
2015-12-01 18:24:23 +0800

    登录和注册

--- www/app/controllers/demo/MainController.js
+++ www/app/controllers/demo/MainController.js
@@ -11,12 +11,24 @@ define(['app'], function (app) {
 		  }).then(function(modal) {
 		    $scope.modal = modal;
 		  });
-
+		  $ionicModal.fromTemplateUrl('app/templates/user/reg.html', {
+		    scope: $scope
+		  }).then(function(modal) {
+		    $scope.regmodal = modal;
+		  });
 		  // Triggered in the login modal to close it
 		  $scope.closeLogin = function() {
 		    $scope.modal.hide();
 		  };
-
+		  $scope.closeReg = function() {
+		    $scope.regmodal.hide();
+		  };		  
+		  // Triggered in the login modal to close it
+		  $scope.reguser = function() {
+	
+		    //$scope.modal.hide();
+		      $scope.regmodal.show();
+		  };
 		  // Open the login modal
 		  $scope.login = function() {
 		    $scope.modal.show();
@@ -24,7 +36,10 @@ define(['app'], function (app) {
 
 		  // Perform the login action when the user submits the login form
 		  $scope.doLogin = function() {
-		    //console.log('Doing login', $scope.loginData);
+		    //console.log('Doing login', $scope.loginData.password);
+		    if(!$scope.loginData.password||!$scope.loginData.username){
+		    	return false;
+		    }
 
 		    // Simulate a login delay. Remove this and replace with your login
 		    // code if using a login system
@@ -45,7 +60,7 @@ define(['app'], function (app) {
 				//alert(data);
 				console.log(data.code);
                 if(data.code==100){
-                    window.location.href = "#/app/chartslist";
+                    window.location.href = "#/app/index";
                 }else{
                     $ionicPopup.alert({
                         title:'登录失败',
@@ -63,5 +78,56 @@ define(['app'], function (app) {
 		      $scope.closeLogin();
 		    }, 1000);
 		  };
+		  // Perform the login action when the user submits the login form
+		  $scope.doReg = function() {
+		    //console.log('Doing login', $scope.loginData.password);
+		    if(!$scope.loginData.password||!$scope.loginData.username||!$scope.loginData.email){
+		    	return false;
+		    }
+
+		    // Simulate a login delay. Remove this and replace with your login
+		    // code if using a login system
+            var shapassword=hex_sha1($scope.loginData.password);
+			$http({
+			   url:window.siteurl+'index/AddUser',
+			   method:"POST",
+			   headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+			   },
+                params:{
+                    'name':$scope.loginData.username,
+                    'pwd':shapassword,
+                    'email':$scope.loginData.email
+                }
+			   //data: $scope.loginData
+			}).success(function(data){
+				//$scope.list=data.concat($scope.list);
+				//alert(data);
+				console.log(data.code);
+                if(data.code==100){
+                   // window.location.href = "#/app/index";
+				
+                    var ss=$ionicPopup.alert({
+                        title:'恭喜',
+                        template: "注册成功！"
+                    });
+                    ss.then(function(res) {
+				$scope.closeReg();
+					  });
+                }else{
+                    $ionicPopup.alert({
+                        title:'注册失败',
+                        template: "检查相关信息！"
+                    });
+                }
+				//alert(JSON.stringify(data));
+	            //window.location.href = "Gulugulus/subMenu";
+	        }).error(function(error){
+				alert(error);
+	        }).finally(function() {
+	                    //$scope.$broadcast('scroll.refreshComplete');
+	        })
+
+		  };		  
      });
 });
